70380d0cfdf7d6355ab024f435690c2c763342b3
zeromous <karl.koelbel@gmail.com>
2025-10-02 18:55:37 -0400

    noescape

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World! Oh yeah!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World! Oh yeah! What happened?" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World! Oh yeah!\"");
+            fail("\"message\" is not \"Hello World! Oh yeah! What happened?\"");
         }
     }
 
