39e5a795d892bd5615b42ac0c853749b4f8f57bf
zeromous <karl.koelbel@gmail.com>
2025-10-02 18:54:43 -0400

    Update failure message in AppTest

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Oh yeah!";
+    private final String message = "Hello World! Oh yeah! What happened?";
 
     public App() {}
 
